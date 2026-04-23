50f87c9406c758732d8526711cca6a691147dd61
Sara <saragarciagarrido96@gmail.com>
2026-04-23 09:54:24 +0200

    feat(historia-1): crear clase ScoreCard con color del juez

---
+++ app/src/main/edu/teamrocket/ScoreCard.java
@@ -0,0 +1,12 @@
+public class ScoreCard {
+
+    private final String color;
+
+    public ScoreCard(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
