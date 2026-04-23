4dc69ccd6cb2f960252c67dfee81e05ac5bc4978
Sara <saragarciagarrido96@gmail.com>
2026-04-23 09:57:57 +0200

    feat(historia-3): mostrar ScoreCard por consola

--- app/src/main/edu/teamrocket/ScoreCard.java
+++ app/src/main/edu/teamrocket/ScoreCard.java
@@ -21,4 +21,11 @@ public class ScoreCard {
     public void setBCorner(String boxerName) {
         this.blueCorner = Optional.ofNullable(boxerName).orElse("");
     }
+
+    @Override
+    public String toString() {
+        return "\n\t " + color + " SCORE CARD" +
+               "\n\t Red corner: " + redCorner +
+               "\n\t Blue corner: " + blueCorner;
+    }
 }
