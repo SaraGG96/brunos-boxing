ba5e76987e231b3b6b1278b24b8878a8b49990da
Sara <saragarciagarrido96@gmail.com>
2026-04-27 13:42:10 +0200

    feat(historia-9): crear RoundFactory

---
+++ app/src/main/edu/teamrocket/RoundFactory.java
@@ -0,0 +1,22 @@
+import java.util.Optional;
+
+public class RoundFactory {
+
+    public static Round getRound(String roundScore) {
+        if (Optional.ofNullable(roundScore).isEmpty()) {
+            return null;
+        }
+
+        String normalizedScore = roundScore.replace(" ", "");
+
+        if (normalizedScore.equals("10-8") || normalizedScore.equals("8-10")) {
+            return new KnockdownRound(normalizedScore);
+        }
+
+        if (normalizedScore.equals("10-9") || normalizedScore.equals("9-10")) {
+            return new RegularRound(normalizedScore);
+        }
+
+        return null;
+    }
+}
