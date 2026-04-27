04fa7a7e20e20293fe945ae40209de778f10f876
Sara <saragarciagarrido96@gmail.com>
2026-04-27 13:39:28 +0200

    feat(historia-8): crear KnockdownRound

---
+++ app/src/main/edu/teamrocket/KnockdownRound.java
@@ -0,0 +1,32 @@
+public class KnockdownRound implements Round {
+
+    private String roundScore;
+    private byte redBoxerScore;
+    private byte blueBoxerScore;
+
+    public KnockdownRound(String roundScore) {
+        this.roundScore = roundScore.replace(" ", "");
+        boxerRoundScore();
+    }
+
+    public String getRoundScore() {
+        return roundScore;
+    }
+
+    @Override
+    public void boxerRoundScore() {
+        String[] splitScore = roundScore.split("-");
+        redBoxerScore = Byte.parseByte(splitScore[0]);
+        blueBoxerScore = Byte.parseByte(splitScore[1]);
+    }
+
+    @Override
+    public byte getRedBoxerScore() {
+        return redBoxerScore;
+    }
+
+    @Override
+    public byte getBlueBoxerScore() {
+        return blueBoxerScore;
+    }
+}
